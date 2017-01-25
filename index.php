ee9b3b0bc5ef4b1bf20178eb3052ddf72a19bfa9
godsend <jcavat@gmail.com>
2017-01-25 15:57:06 +0100

    Add GET for maps and maps id

--- index.php
+++ index.php
@@ -8,35 +8,58 @@ $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 $method = $_SERVER['REQUEST_METHOD'];
 $action = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
 $key = array_shift($request);
+$credentials = require('credentials.php');
 
 $rep = array();
 
-switch ($method) {
-case 'GET':
+try {
+    $db = new PDO('mysql:host='.$credentials['dbhost'].';dbname='.$credentials['dbname'], $credentials['dbuser'], $credentials['dbpass'], array(
+        PDO::ATTR_PERSISTENT => true
+    ));
 
-    switch ($action) {
-    case 'maps':
-        if ($key) {
-            // if the id of the map is provided
-            $rep = array('id' => $key, 'name' => 'map with name');
-        } else {
-            // otherwise we provide the full list
-            $rep = array(array('id' => 1, 'name'=>'toto'), array('id' => 2, 'name' => 'titi'));
+    switch ($method) {
+    case 'GET':
+
+        switch ($action) {
+        case 'maps':
+            if ($key) {
+                // if the id of the map is provided
+                $rep = array('id' => $key, 'name' => 'map with name');
+
+                $preparedStatement = $db->prepare('SELECT * FROM `maps` WHERE uid = :uid');
+                $preparedStatement->bindValue('uid', $key, PDO::PARAM_INT);
+                $preparedStatement->execute();
+
+                $rep = $preparedStatement->fetch(PDO::FETCH_ASSOC);
+
+            } else {
+                // otherwise we provide the full list
+                //$rep = array(array('id' => 1, 'name'=>'toto'), array('id' => 2, 'name' => 'titi'));
+                $preparedStatement = $db->prepare('SELECT uid, title, city, creationDate FROM `maps`');
+                $preparedStatement->execute();
+
+                $rep = $preparedStatement->fetchAll(PDO::FETCH_ASSOC);
+
+            }
+            break;
         }
+
         break;
+
+        case 'POST':
+            /*
+             * insert a map in the DB and get back the id
+             */
+            $rep = array('id' => 33);
+            break;
+
     }
+    echo json_encode( $rep, JSON_NUMERIC_CHECK);
 
-    break;
 
-case 'POST':
-    /*
-     * insert a map in the DB and get back the id
-     */
-    $rep = array('id' => 33);
-    break;
-    
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    return;
 }
-echo json_encode( $rep, JSON_NUMERIC_CHECK);
-
 
 ?>
